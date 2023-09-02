0073c1333a0cec4f5ce55b8d240f3f9bc96f3903
Josh <101682378+dddddsfdfd@users.noreply.github.com>
2023-09-02 09:53:48 +0800

    Add files via upload
    嘗試看看

---
+++ Basic_arithmetic.py
@@ -0,0 +1,14 @@
+#Basic_arithmetic.py
+
+print(3+8)
+print(4-2*5)
+print((4-2)**5) #**表示平方
+print(3**(5/2)) #/包含小數點
+print(3**(5//2)) #//不包含小數點
+
+> bigger than
+< smaller than
+>= not small than
+<= not bigger than
+== equals
+!= not equals
